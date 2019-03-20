a689b974ac689ddcbb56ca3ca560191f27a1d541
Gabriel Mulligan <48606956+gabrielmulligan@users.noreply.github.com>
2019-03-20 23:13:03 +0000

    Delete tuesday.py

--- tuesday.py
+++
@@ -1,9 +0,0 @@
-# Ian McLoughlin, 2018-02-01
-# Is it Saturday?
-
-import datetime
-
-if datetime.datetime.today().weekday() == 5:
-  print("Yay! It is Saturday.")
-else:
-  print("Unfortunately it is not Saturday.")
